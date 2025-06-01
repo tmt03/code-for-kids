af9a3851b289859ec301ae503176d8152ad0a1b0
DuyHaCode <starplatinum3172005@gmail.com>
2025-06-01 21:15:40 +0700

    Sửa String thành function trong questMap và đổi tên asset

--- frontend/game/preloadAssets.ts
+++ frontend/game/preloadAssets.ts
@@ -6,13 +6,13 @@ export function preloadAssets(scene: Phaser.Scene) {
 
   // Chap 0 nhiem vu 1
   scene.load.image("background_no_color", "/sky/no_color_sky_1.png");
-  scene.load.image("sky_1", "/sky/sky_1.png");
-  scene.load.image("sky_2", "/sky/sky_2.png");
-  scene.load.image("sky_3", "/sky/sky_3.png");
-  scene.load.image("sky_4", "/sky/sky_4.png");
-  scene.load.image("sky_5", "/sky/sky_5.png");
-  scene.load.image("sky_6", "/sky/sky_6.png");
-  scene.load.image("sky_7", "/sky/sky_7.png");
+  scene.load.image("blue_sky", "/sky/sky_1.png");
+  scene.load.image("midnight_sky", "/sky/sky_2.png");
+  scene.load.image("pink_sky", "/sky/sky_3.png");
+  scene.load.image("cloudy_sky", "/sky/sky_4.png");
+  scene.load.image("afternoon_sky", "/sky/sky_5.png");
+  scene.load.image("sunset_sky", "/sky/sky_6.png");
+  scene.load.image("night_sky", "/sky/sky_7.png");
 
   // Chap 0 nhiem vu 2
   scene.load.image("ground_1", "/ground/ground_1.png");
@@ -22,33 +22,32 @@ export function preloadAssets(scene: Phaser.Scene) {
   scene.load.image("castle", "/castle/castle.png");
 
   // Chap 1 nhiem vu 3
-  scene.load.image("tree_1", "/tree/tree_1.png");
-  scene.load.image("tree_2", "/tree/tree_2.png");
-  scene.load.image("tree_3", "/tree/tree_3.png");
-  scene.load.image("tree_4", "/tree/tree_4.png");
+  scene.load.image("pine_tree", "/tree/tree_1.png");
+  scene.load.image("tree", "/tree/tree_2.png");
+  scene.load.image("small_tree", "/tree/tree_3.png");
+  scene.load.image("big_tree", "/tree/tree_4.png");
 
   // Chap 1 thu thach
-  scene.load.image("shrub_1", "/tree/shrub_1.png");
-  scene.load.image("shrub_2", "/tree/shrub_2.png");
-  scene.load.image("shrub_3", "/tree/shrub_3.png");
-  scene.load.image("shrub_4", "/tree/shrub_4.png");
-  scene.load.image("rock_1", "/rock/rock_1.png");
-  scene.load.image("rock_2", "/rock/rock_2.png");
-  scene.load.image("rock_3", "/rock/rock_3.png");
-  scene.load.image("rock_4", "/rock/rock_4.png");
+  scene.load.image("small_shrub", "/tree/shrub_1.png");
+  scene.load.image("cherry_shrub", "/tree/shrub_2.png");
+  scene.load.image("berry_shrub", "/tree/shrub_3.png");
+  scene.load.image("big_shrub", "/tree/shrub_4.png");
+  scene.load.image("small_rock", "/rock/rock_1.png");
+  scene.load.image("tall_rock", "/rock/rock_2.png");
+  scene.load.image("strike_rock", "/rock/rock_3.png");
+  scene.load.image("big_rock", "/rock/rock_4.png");
   scene.load.image("mountain_1", "/mountain/mountain_1.png");
-  scene.load.image("mountain_2", "/mountain/mountain_2.png");
-  scene.load.image("mountain_3", "/mountain/mountain_3.png");
-  scene.load.image("mountain_4", "/mountain/mountain_4.png");
-  scene.load.image("mountain_5", "/mountain/mountain_5.png");
-  scene.load.image("mountain_6", "/mountain/mountain_6.png");
-  scene.load.image("forest_1", "/forest/forest_1.png");
-  scene.load.image("forest_2", "/forest/forest_2.png");
-  scene.load.image("forest_3", "/forest/forest_3.png");
+  scene.load.image("ice_mountain", "/mountain/mountain_2.png");
+  scene.load.image("meadow_mountain", "/mountain/mountain_3.png");
+  scene.load.image("plain_mountain", "/mountain/mountain_4.png");
+  scene.load.image("snow_mountain", "/mountain/mountain_5.png");
+  scene.load.image("dirt_mountain", "/mountain/mountain_6.png");
+  scene.load.image("plain_forest", "/forest/forest_1.png");
+  scene.load.image("dark_forest", "/forest/forest_2.png");
+  scene.load.image("night_forest", "/forest/forest_3.png");
   scene.load.image("forest_4", "/forest/forest_4.png");
-  scene.load.image("forest_5", "/forest/forest_5.png");
-  scene.load.image("forest_6", "/forest/forest_6.png");
-  scene.load.image("forest_7", "/forest/forest_7.png");
+  scene.load.image("fog_forest", "/forest/forest_5.png");
+  scene.load.image("sunset_forest", "/forest/forest_6.png");
 
   // Chap 2 nhiem vu 1, 2, 3
   scene.load.spritesheet("player_run", "/player/knight_1/Run.png", {
