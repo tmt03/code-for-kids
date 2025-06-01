1c17cfe25fb6bd1ee8bd0204bf2698046209bf7d
tamtmhe176890 <tamtmhe176890@fpt.edu.vn>
2025-06-01 14:15:18 +0700

    xong phân quyền cho BE

--- backend/src/routes/v1/chapterRoute.ts
+++ backend/src/routes/v1/chapterRoute.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { chapterController } from "../../controllers/chapterController";
+import { verifyToken } from "../../middlewares/authMiddleware";
+import { requirePermission } from "../../middlewares/permissionMiddleware";
 
 const Router = express.Router();
-Router.route("/").get(chapterController.getAllChapters);
+Router.get(
+  "/",
+  verifyToken,
+  requirePermission("viewChapter"), // 🎯 user & admin đều có thể xem danh sách chapter
+  chapterController.getAllChapters
+);
 
 export const chapterRoute = Router;
